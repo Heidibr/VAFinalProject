6f9394c4c7d2a4e8e5a4eaeb1e89c47db748c222
Heidibr <heidilb@stud.ntnu.no>
2019-12-16 13:46:06 +0100

    sorting table with dropdown works

--- js/table.js
+++ js/table.js
@@ -4,75 +4,71 @@ var dd = d3.select("#dd")
 d3.csv("./data/convertcsv.csv", function(data){
 
 // List of groups (here I have one group per column)
-var allGroup = d3.selectAll('option')
+var allGroup = d3.selectAll('option');
 
 // add the options to the button
 d3.select("#dd")
   .selectAll('myOptions')
-   .data(allGroup)
+  .data(allGroup)
   .enter()
   .append('option')
   .text(function (d) { return d; }) // text showed in the menu
   .attr("value", function (d) { return d; })
 
-  // A function that update the chart
-  function update(selectedGroup) {
-    // Create new data with the selection?
-    var dataFilter = data.map(function(d){return d.ISrank})
-    sorted = sortByRank(dataFilter)
-
-
-  }
-
-  function sortByRank(rank){
-    sorted = rank.sort((a, b) => a - b)
-    console.log(sorted)
-  }
-        
-  // When the button is changed, run the updateChart function
-  d3.select("#dd").on("click", function(d) {
+  // When the button is changed, we run the redraw to get the newly sorted data
+  d3.select("#dd").on("change", function(d) {
       // recover the option that has been chosen
       var selectedOption = d3.select(this).property("value")
       console.log(selectedOption)
       // run the updateChart function with this selected option
-      update(selectedOption)
+      rows.sort( (a, b) => a[selectedOption] - b[selectedOption]);
+      redraw(0)
   })
 
+  
   var body = d3.select("#table-holder");
+  //Eventuelt sette columns statisk slik at vi får fullt navn på dem??
+  var titles = d3.keys(data[0])
   var table = body.append("table");
   var thead = table.append("thead")
   var tbody = table.append("tbody");
-    var columns = data.columns;
+
     header = thead.append("tr")
       .selectAll("th")
-      .data(columns)
+      .data(titles)
       .enter()
       .append("th")
       .text(function(d,i){
         return d;
-    });
+    })
+
     var rows = tbody.selectAll("tr")
       .data(data)
       .enter()
       .append("tr")
 
+      console.log(rows)
     var cells = rows.selectAll("td")
-      .data(function(row){
-        return columns.map(function(column){
+      .data(function(d){
+        return titles.map(function(column){
           //shows countries. 
           //console.log(row["Country"])
           return{
-            column:column,
-            value:row[column]
+           'value':d[column],
+            name: column
           }
         })
       })
       .enter()
       .append("td")
-      .text(function(d,i){
+      .attr('data-th',function(d,i){
+        return d.name
+      })
+      .text(function(d){
         return d.value
       })
 
+      
       d3.select("#buttons").datum({portion : 0});
       // the chain select here pushes the datum onto the up and down buttons also
       d3.select("#buttons").select("#up").on ("click", function(d) {
