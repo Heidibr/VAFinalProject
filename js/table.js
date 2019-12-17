4ecdd7546763ba35c514f2260c95d4524a7d6431
Heidibr <heidilb@stud.ntnu.no>
2019-12-17 12:18:09 +0100

    plot fpr PCA

--- js/table.js
+++ js/table.js
@@ -1,11 +1,320 @@
+margin_scatter = {
+  top: 20,
+  right: 20,
+  bottom: 40,
+  left: 40
+},
+  width_scatter = 380 - margin_scatter.left - margin_scatter.right,
+  height_scatter = 300.16 - margin_scatter.top - margin_scatter.bottom,
 
-var dd = d3.select("#dd")
+  x_scatter = d3.scaleLinear().range([0, width_scatter]),
+  y_scatter = d3.scaleLinear().range([height_scatter, 0]),
 
-d3.csv("./data/convertcsv.csv", function(data){
+  xAxis_scatter = d3.axisBottom(x_scatter),
+  yAxis_scatter = d3.axisLeft(y_scatter);
+
+
+svg = d3.select("#scatterArea").append("svg")
+  .attr("width", width_scatter + margin_scatter.left + margin_scatter.right)
+  .attr("height", height_scatter + margin_scatter.top + margin_scatter.bottom)
+  .attr("transform", "translate(" + 20 + "," + 0 + ")");
+
+focus = svg.append("g")
+  .attr("class", "focus")
+  .attr("transform", "translate(" + margin_scatter.left + "," + margin_scatter.top + ")");
+
+xAxis = focus.append("g")
+  .attr("class", "axis axis--x")
+  .attr("transform", "translate(0," + height_scatter + ")")
+  .call(xAxis_scatter);
+
+yAxis = focus.append("g")
+  .attr("class", "axis axis--y")
+  .call(yAxis_scatter);
+
+focus.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin_scatter.left)
+  .attr("x", 0 - (height_scatter / 2))
+  .attr("dy", "1em")
+  .style("text-anchor", "middle")
+  .text("Y1");
+
+svg.append("text")
+  .attr("transform",
+      "translate(" + ((width_scatter + margin_scatter.right + margin_scatter.left) / 2) + " ," +
+      (height_scatter + margin_scatter.top + margin_scatter.bottom) + ")")
+  .style("text-anchor", "middle")
+  .text("Y2");
+
+
+function drawScatter(data) {
+  cValue = function (d) {
+      return d.continent;
+  }
+  
+  color = d3.scaleOrdinal(d3.schemeCategory10);
+  //Update the scale
+  var maxHeight = d3.max(data, function (d) {
+      return Math.abs(d.Y2)
+  });
+  var minHeight = d3.min(data, function (d) {
+      return Math.abs(d.Y2)
+  })
+  y_scatter.domain([maxHeight + 0.5, -maxHeight - 0.5]); //show negative
+  var maxWidth = d3.max(data, function (d) {
+      return Math.abs(d.Y1)
+  });
+  var minWidth = d3.min(data, function (d) {
+      return Math.abs(d.Y1)
+  })
+  x_scatter.domain([maxWidth + 0.5, -maxWidth - 4]); //show negative
+
+  // Update axes
+  xAxis_scatter.scale(x_scatter);
+  xAxis.transition().call(xAxis_scatter);
+  yAxis_scatter.scale(y_scatter);
+  yAxis.transition().call(yAxis_scatter);
+
+  focus.selectAll("circle").remove();
+
+  dots = focus.selectAll("circle").data(data);
+
+  dots.enter().append("circle")
+      .attr("r", 3)
+      .attr("class", "knncircle")
+      .style("fill", function (d) {
+         
+              return color(cValue(d));
+  
+      })
+      .attr("cx", function (d) {
+          return x_scatter(d.Y1);
+      })
+      .attr("cy", function (d) {
+          return y_scatter(d.Y2)
+      })
+      
+
+  var legend = focus.selectAll(".legend")
+      .data(color.domain())
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function (d, i) {
+          return "translate(0," + i * 17 + ")";
+      });
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", width_scatter - 5)
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", color);
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", width_scatter - 15)
+      .attr("y", 6)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .style("font-size", "0.8em")
+      .text(function (d) {
+          return d;
+      })
+}
+
+////////////////////////// Brush initialization (scatter plot) /////////////////////////////////
+
+brushTot = d3.brush()
+    .extent([
+        [0, 0],
+        [width_scatter, height_scatter]
+    ])
+    .on("end", selected)
+
+focus.append("g")
+    .attr("class", "brushT")
+    .call(brushTot);
+
+///////////////////////selection function of brush (scatter plot) ///////////////////////////////
+
+function selected() {
+    dataSelection = []
+
+    var selection = d3.event.selection;
+
+    if (selection != null) {
+        focus.selectAll("circle")
+            .style("fill", function (d) {
+                if (x_scatter(d.Y1) > selection[0][0] && x_scatter(d.Y1) < selection[1][0] &&
+                    y_scatter(d.Y2) > selection[0][1] && y_scatter(d.Y2) < selection[1][1]) {
+
+                    dataSelection.push(d);
+                    return "green";
+                } else {
+                    return "red";
+                }
+            })
+
+       // g.selectAll("circle").remove();
+
+        /*
+            g.selectAll("circle")
+                .data(dataSelection)
+                .enter().append("circle", "image")
+                .attr("r", 2)
+                .style("fill", "red")
+                .on("mousemove", showTooltip2)
+                .on("mouseout", hideTooltip2)
+                .attr("transform", function (d) {
+                    return "translate(" + projection([
+                        d.lon,
+                        d.lat,
+                    ]) + ")";
+                })
+        //} else {
+            g.selectAll("circle")
+                .data(dataSelection)
+                .enter().append("circle", "image")
+                .attr("r", 8 / k)
+                .style("fill", "green")
+                .on("mousemove", showTooltip2)
+                .on("mouseout", hideTooltip2)
+
+                .attr("transform", function (d) {
+
+                    return "translate(" + projection([
+                        d.lon,
+                        d.lat,
+
+                    ]) + ")";
+                })
+        /}*/
+       // RadarChart(".radarChart", dataSelection, radarChartOptions);
+        //lineChart(dataSelection);
+      console.log(dataSelection)
+      
+      }
+    //Nothing is selected by the brush
+    else {
+        dataSelection = [];
+
+        focus.selectAll("circle")
+            .style("fill", function (d) {
+                return color(cValue(d));
+            })
+
+        console.log("reset");
+
+        if (flag_s == false) {
+            datap = filtered_data;
+        } else {
+            datap = contained_points;
+        }
+
+        g.selectAll("circle").remove();
+
+        if (flag_s == false) {
+            g.selectAll("circle")
+                .data(datap)
+                .enter().append("circle", "image")
+                .attr("r", 2)
+                .style("fill", "red")
+                .on("mousemove", showTooltip2)
+                .on("mouseout", hideTooltip2)
+
+                .attr("transform", function (d) {
+                    return "translate(" + projection([
+                        d.lon,
+                        d.lat,
+                    ]) + ")";
+                })
+        } else {
+            g.selectAll("circle")
+                .data(datap)
+                .enter().append("circle", "image")
+                .attr("r", 8 / k)
+                .style("fill", "red")
+                .on("mousemove", showTooltip2)
+                .on("mouseout", hideTooltip2)
+
+                .attr("transform", function (d) {
+
+                    return "translate(" + projection([
+                        d.lon,
+                        d.lat,
+
+                    ]) + ")";
+                })
+        }
+        //  remove the rader cgart when click on the empty area of the scatter plot
+        //onceForRadar_flag = false;
+        //RadarChart(".radarChart", onceForRadar, radarChartOptions, onceForRadar_flag);
+        // remove the table
+        //d3.selectAll(".table-remove").remove();
+        //$(".html_table").css("display", "table");
+        // remove the knn lines
+        //focus.selectAll(".knnline").remove()
+        //lineChart(null);
+        //remove old lines 
+        
+
+        // put back the legend
+
+        var legend = focus.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+            return "translate(0," + i * 17 + ")";
+        });
+
+    // draw legend colored rectangles
+    legend.append("rect")
+        .attr("x", width_scatter - 5)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", color);
+
+    // draw legend text
+    legend.append("text")
+        .attr("x", width_scatter - 15)
+        .attr("y", 6)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .style("font-size", "0.8em")
+        .text(function (d) {
+            return d;
+        })
+    }
+}
+
+
+
+/////////////////////////////////////////TABLE CODE!!!!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////////TABLE CODE!!!!\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/*var currentCountry;
+
+function getCountry(name){
+  if(currentCountry != name){
+    currentCountry = name;
+    nameChanged()
+  } else {
+    return false;
+  }
+  
+}
+function nameChanged(){
+  console.log(currentCountry)
+  return true;
+  
+}*/
+
+/////////////////////////////////////////TABLE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function drawTable(data){
 
 // List of groups (here I have one group per column)
 var allGroup = d3.selectAll('option');
-
 // add the options to the button
 d3.select("#dd")
   .selectAll('myOptions')
@@ -15,6 +324,7 @@ d3.select("#dd")
   .text(function (d) { return d; }) // text showed in the menu
   .attr("value", function (d) { return d; })
 
+  
   // When the button is changed, we run the redraw to get the newly sorted data
   d3.select("#dd").on("change", function(d) {
       // recover the option that has been chosen
@@ -25,6 +335,12 @@ d3.select("#dd")
       redraw(0)
   })
 
+  /*d3.select('#current').on('change', function(d){
+    var changesText= d3.select(this).property("value")
+    console.log(changesText)
+  })*/
+  
+  
   
   var body = d3.select("#table-holder");
   //Eventuelt sette columns statisk slik at vi får fullt navn på dem??
@@ -52,7 +368,6 @@ d3.select("#dd")
       .data(function(d){
         return titles.map(function(column){
           //shows countries. 
-          //console.log(row["Country"])
           return{
            'value':d[column],
             name: column
@@ -80,6 +395,7 @@ d3.select("#dd")
         redraw (d.portion);
       })
       
+      
 
 
       function redraw (start) {
@@ -90,7 +406,16 @@ d3.select("#dd")
       }
       redraw(0);
     
+}
+
+d3.json("./data/csvjsonPCA.json", function(data){
+var fullDataSet = data
+console.log(data)
+console.log(data[1].Y1)
+drawTable(fullDataSet)
+drawScatter(fullDataSet)
+
+
       
-    
 })
 
